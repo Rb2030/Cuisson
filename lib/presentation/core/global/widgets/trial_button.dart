1d28b892a18a3bb0fc3340575c1fe6053273769d
Rb2030 <rossmail2050@gmail.com>
2020-09-13 02:49:26 +0100

    Add BlocProvider.value in the trialButton as this correct way to reference the cubit.

--- lib/presentation/core/global/widgets/trial_button.dart
+++ lib/presentation/core/global/widgets/trial_button.dart
@@ -5,23 +5,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class TrialButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ThemeCubit, ThemeState>(
-        listener: (context, ThemeState state) {},
-        builder: (context, state) {
-          return Container(
-            width: 100,
-            height: 50,
-            child: RaisedButton(
-              color: Colors.yellow,
-              textColor: Colors.white,
-              onPressed: () {
-                debugPrint("button pressed");
-                final themeCubit = context.bloc<ThemeCubit>();
-                themeCubit.changeTheme();
-              },
-              child: const Text('PRESS'),
-            ),
-          );
-        });
+    return BlocProvider.value(
+      value: BlocProvider.of<ThemeCubit>(context),
+      child: Container(
+        width: 100,
+        height: 50,
+        child: RaisedButton(
+          color: Colors.yellow,
+          textColor: Colors.white,
+          onPressed: () {
+            debugPrint("button pressed");
+            final themeCubit = context.bloc<ThemeCubit>();
+            themeCubit.changeTheme();
+          },
+          child: const Text('PRESS'),
+        ),
+      ),
+    );
   }
 }
